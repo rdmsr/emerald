dc08c6ec428268dd45916ffd871201f190c2f27f
Abb1x <abb1x@protonmail.com>
2021-01-03 16:48:17 -0500

    [multitasking] Added registers and typedefs
    I added typedefs to task.h to prepare for the multitasking I will
    probably do tomorrow

--- src/proc/task.h
+++ src/proc/task.h
@@ -4,10 +4,25 @@
 #include <stdint.h>
 typedef struct
 {
+  uint64_t rax,rbx,rcx,rdx,rsi,rdi,rbp,rsp,r8,r9,r10,r11,r12,r13,r14,r15;
+} regs64_t;
+
+typedef struct
+{
+  uint8_t priority = 0;
+  uint32_t time_slice;
+  regs64_t registers;
+} thread_t;
+
+typedef struct
+{
     uint64_t virtual_adress;
     int id;
+    thread_t thread;
     char name[];
 } process_t;
 
+void EmeraldProc_timer_init();
+void EmeraldProc_change_priority(uint8_t priority);
 
 #endif
