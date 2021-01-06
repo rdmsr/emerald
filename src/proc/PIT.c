e8f74bedb64fd6eac8b9419c833221d481956a09
Abb1x <abb1x@protonmail.com>
2021-01-06 13:44:38 -0500

    [proc] Created PIT
    New files PIT.h and PIT.c

---
+++ src/proc/PIT.c
@@ -0,0 +1,11 @@
+#include "PIT.h"
+#include <libasm/asm.h>
+#include <debug-utilities/logger.h>
+void EmeraldProc_PIT_init(uint32_t frequency)
+{
+  uint16_t divisor = 1193182 / frequency;
+  EmeraldASM_outb(0x43,0x36);
+  EmeraldASM_outb(0x40, (uint8_t)divisor & 0xFF);
+  EmeraldASM_outb(0x40, (uint8_t)(divisor >> 8) & 0xFF);
+  log(INFO,"Initialized PIT");
+}
---
+++ src/proc/PIT.h
@@ -0,0 +1,6 @@
+#ifndef PIT_H
+#define PIT_H
+
+void EmeraldProc_PIT_init(uint32_t frequency);
+
+#endif
