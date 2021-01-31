c1f6ad8209be37fc4b94bfbe8cc3d9d1007e3e93
Abb1x <abb1x@protonmail.com>
2021-01-31 18:35:05 -0500

    VMM: added high offset

--- kernel/main.c
+++ kernel/main.c
@@ -64,7 +64,7 @@ void kmain(struct stivale2_struct *info)
 
     PMM_init((void *)boot_info->memory_map, boot_info->memory_map->entries);
 
-    /*VMM_init();*/
+    VMM_init();
 
     VBE_puts("\nWelcome to ", white);
     VBE_puts("EmeraldOS!\n", green);
--- kernel/memory/vmm.h
+++ kernel/memory/vmm.h
@@ -30,7 +30,7 @@
 #include <libk/logging.h>
 #include <stdbool.h>
 #include <stdint.h>
-
+#define HIGH_OFFSET 0xffff800000000000
 typedef struct
 {
     uintptr_t *pml4;
