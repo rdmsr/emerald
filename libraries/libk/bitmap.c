c3765461e30c17efa000309113f137c46b77ff37
Abb1x <abb1x@protonmail.com>
2021-03-07 16:28:25 -0500

    libk: WIP bitmap header files

---
+++ libraries/libk/bitmap.c
@@ -0,0 +1,62 @@
+#include <libk/bitmap.h>
+#include <libk/logging.h>
+#include <memory/pmm.h>
+#include <system/panic.h>
+
+static void set(size_t index, bool value, Bitmap *self)
+{
+    if (index > self->size)
+    {
+        log(ERROR, "Trying to set bit outside of the bitmap!");
+
+        panic("Trying to set bit outside of the bitmap!");
+
+        return;
+    }
+
+    size_t bit = index % 8; /* 8 since the bitmap is uint8_t */
+    size_t byte = index / 8;
+
+    if (value)
+    {
+        bitmap->buffer[byte] |= (1 << (bit));
+    }
+    else
+    {
+        bitmap->buffer[byte] &= ~(1 << (bit));
+    }
+}
+
+static bool get(size_t index, Bitmap *self)
+{
+    if (index > self->size)
+    {
+        log(ERROR, "Trying to get bit outside of the bitmap!");
+
+        panic("Trying to get bit outside of the bitmap!");
+
+        return false;
+    }
+
+    size_t bit = index % 8; /* 8 since the bitmap is uint8_t */
+    size_t byte = index / 8;
+
+    return (bitmap->buffer[byte] & (1 << (bit)));
+}
+
+Bitmap _Bitmap(uint8_t *data, size_t size)
+{
+    module("Bitmap");
+
+    log(INFO, "Creating bitmap %x which is %d bytes", (uint64_t)data, size / 8);
+
+    memset(data, 0xff, size / 8);
+
+    Bitmap new_bitmap;
+
+    new_bitmap.size = size;
+    new_bitmap.buffer = data;
+    new_bitmap.last_free = 0;
+    new_bitmap.set = set;
+    new_bitmap.get = get;
+}
---
+++ libraries/libk/bitmap.h
@@ -0,0 +1,27 @@
+#ifndef BITMAP_H
+#define BITMAP_H
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+typedef struct bitmap_struct
+{
+
+    /* Functions */
+    void (*set)(size_t, bool, struct bitmap_struct *); /* It's a bool because bits are only 0 or 1 */
+    bool (*get)(size_t, struct bitmap_struct *);
+
+    size_t (*find_free)(size_t, struct bitmap_struct *);
+    size_t (*alloc)(size_t, struct bitmap_struct *);
+    size_t (*set_free)(size_t, size_t, struct bitmap_struct *);
+    size_t (*set_used)(size_t, size_t, struct bitmap_struct *);
+
+    /* Variables */
+    size_t bitmap_size, last_free;
+    uint8_t *buffer;
+
+} Bitmap;
+
+Bitmap _Bitmap(uint8_t *data, size_t size);
+
+#endif
