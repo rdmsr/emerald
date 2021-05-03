b4b1c5052141d43156bfb292d9f83a71dacd560b
Abb1x <abb1x@protonmail.com>
2021-05-03 09:50:08 -0400

    Framebuffer: documented code

--- kernel/devices/video/framebuffer.h
+++ kernel/devices/video/framebuffer.h
@@ -31,29 +31,78 @@
 #include <boot/stivale2.h>
 #include <stddef.h>
 
+/**
+A struct containing a RGB color.
+*/
 typedef struct
 {
-    uint8_t r, g, b;
+    /** Red */
+    uint8_t r;
+    /** Green */
+    uint8_t g;
+    /** Blue */
+    uint8_t b;
 } Color;
 
+/** A struct containing information about a framebuffer. */
 typedef struct
 {
-    Color colorscheme[8], background, foreground;
+    /** The colorscheme currently used. */
+    Color colorscheme[8];
+    /** Background color. */
+    Color background;
+    /** Foreground color. */
+    Color foreground;
+    /** The bootloader information about the framebuffer. */
     struct stivale2_struct_tag_framebuffer *fb_info;
+    /** Position of the cursor. */
     int cursor_y, cursor_x;
+    /** PSF font. */
     PSF font;
 } Framebuffer;
 
+/** Creates a new framebuffer.
+@param[in] colorscheme[8] The RGB colorscheme.
+@param[in] info The bootloader information about the framebuffer.
+@param[out] fb The newly created framebuffer.*/
 Framebuffer Framebuffer_init(Color colorscheme[8], struct stivale2_struct *info);
 
+/** Clears the screen */
 void Framebuffer_clear();
+
+/** Puts a character on the framebuffer
+@param[in] c The character.
+@param[in] position_x The x position of the character.
+@param[in] position_y The y position of the character.
+@param[in] self The framebuffer.*/
+
 void Framebuffer_putc(char c, int position_x, int position_y, Framebuffer *self);
+
+/** Puts a string on the framebuffer
+@param[in] string The string.*/
+
 void Framebuffer_puts(char *string);
+
+/** Sets the currently used framebuffer (every function will now use this one)
+@param[in] self The framebuffer to set as currently used framebuffer */
+
 void Framebuffer_set(Framebuffer *self);
+
+/** Puts a character using the cursor
+@param[in] c The character. */
+
 void Framebuffer_put(char c);
 
+/** Generates a RGB color
+@param[in] r Red.
+@param[in] g Green.
+@param[in] b Blue.
+@param[out] color The generated color.*/
 Color rgb(int r, int g, int b);
 
+/** GUI Log
+@param[in] level The level of logging.
+@param[in] format The formatted string.*/
 void glog(int level, char *format, ...);
 
 enum glog_level
