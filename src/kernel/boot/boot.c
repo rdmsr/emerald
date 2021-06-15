e9a4bb378378b8a54137623d4401740eb57f94c1
Abb1x <abb1x@protonmail.com>
2021-06-15 16:59:52 -0400

    Feat: add stivale2_get_tag to boot.c

--- src/kernel/boot/boot.c
+++ src/kernel/boot/boot.c
@@ -5,6 +5,7 @@
  */
 
 #include <main.h>
+#include <boot/boot.h>
 
 static uint8_t stack[4096] = {0};
 
@@ -32,3 +33,21 @@ __attribute__((section(".stivale2hdr"), used)) static struct stivale2_header sti
     .flags = (1 << 1),
     .tags = (uintptr_t)&framebuffer_hdr_tag,
 };
+
+
+void *stivale2_get_tag(struct stivale2_struct *stivale2_struct, uint64_t id)
+{
+    struct stivale2_tag *current_tag = (void *)stivale2_struct->tags;
+
+    while (current_tag)
+    {
+        if (current_tag->identifier == id)
+        {
+            return current_tag;
+        }
+
+        current_tag = (void *)current_tag->next;
+    }
+
+    return NULL;
+}
