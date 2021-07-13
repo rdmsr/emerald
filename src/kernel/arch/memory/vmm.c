5fd7bb64d4abc26c9a75077d4f426618edbccab3
Abb1x <abb1x@protonmail.com>
2021-07-13 08:50:15 -0400

    Fix: add more logging to vmm

--- src/kernel/arch/memory/vmm.c
+++ src/kernel/arch/memory/vmm.c
@@ -88,8 +88,9 @@ void vmm_initialize(struct stivale2_struct *stivale2_struct)
             }
         }
 
-        log(INFO, "Mapped {i} / {i} of the memory map", i + 1, memory_map->entries);
+        log(INFO, "Mapped {i}/{i} of the memory map", i + 1, memory_map->entries);
     }
+    log(INFO, "Mapped memory map");
 
     load_pagemap(kernel_pagemap);
 
