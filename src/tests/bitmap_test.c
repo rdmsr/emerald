e20e7e9317b99e0c6d047bb16e88ed8d135eb5e6
Abb1x <abbix@riseup.net>
2021-07-31 11:50:09 -0400

    Feat: add bitmap tests

---
+++ src/tests/bitmap_test.c
@@ -0,0 +1,30 @@
+#include <emerald/debug.h>
+#include <emerald/ds/bitmap.h>
+
+uint8_t buf[20];
+
+static Bitmap bitmap = {.size = 20, .data = buf};
+
+TEST(bitmap_fill)
+{
+    bitmap_fill(&bitmap, 1);
+
+    UNIT_ASSERT_EQUAL(bitmap_get(&bitmap, 0), 1);
+}
+
+TEST(bitmap_get)
+{
+    UNIT_ASSERT_EQUAL(bitmap_get(&bitmap, 0), 1);
+}
+
+TEST(bitmap_clear)
+{
+    bitmap_clear(&bitmap, 0);
+    UNIT_ASSERT_EQUAL(bitmap_get(&bitmap, 0), 0);
+}
+
+TEST(bitmap_set)
+{
+    bitmap_set(&bitmap, 0);
+    UNIT_ASSERT_EQUAL(bitmap_get(&bitmap, 0), 1);
+}
